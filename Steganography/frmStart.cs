a2fb8ab2006d8f753ba09d074c0a848808d64940
Sajjad Aemmi <sajjadaemmi@gmail.com>
2018-09-07 22:13:58 +0430

    Add files via upload

---
+++ Steganography/frmStart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steganography
+{
+    public partial class frmStart : Form
+    {
+        public frmStart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form = new frmMain();
+            form.Closed += (s, args) => this.Close();
+            form.Show();
+        }
+
+        private void frmStart_Load(object sender, EventArgs e)
+        {
+            this.Icon = Steganography.Properties.Resources.icon;
+        }
+    }
+}
